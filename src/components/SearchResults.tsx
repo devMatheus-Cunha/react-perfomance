dd05fe58b226cf5dd89e7ddf4bcfadd1bbb5ee3f
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-01-04 16:21:38 -0300

    feat: using useMemo

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ProductItem from "./ProductItem";
+import React, { useMemo } from "react";
+import { ProductItem } from "./ProductItem";
 
 // interfce
 interface SearchResultsProps {
@@ -8,16 +8,34 @@ interface SearchResultsProps {
     price: number;
     title: string;
   }[];
+  addToWishlist: (id: number) => void;
 }
 
-const SearchResults = ({ results }: SearchResultsProps) => {
+const SearchResults = ({ results, addToWishlist }: SearchResultsProps) => {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
   return (
     <div>
+      <h2>Total R$ {totalPrice}</h2>
+
       {results.map((product) => {
-        return <ProductItem product={product} />;
+        return (
+          <ProductItem
+            key={product.id}
+            product={product}
+            addToWishlist={addToWishlist}
+          />
+        );
       })}
     </div>
   );
 };
 
 export default SearchResults;
+
+//? When to use
+//! Avoid complex re-calculations
+//! Referential equality (when we pass on that information to a child component)
