48c65842e42c8a83b3e649da48d8e75e3bc87434
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-01-05 10:36:28 -0300

    feat: new formated values

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import SearchResults from "./components/SearchResults";
 
 // types
 type ApiData = {
-  id: number;
-  price: number;
-  title: string;
+  totalPrice: number;
+  data: {
+    id: number;
+    price: number;
+    title: string;
+  }[];
 };
 
 //--------------------------
@@ -15,7 +18,10 @@ type ApiData = {
 //--------------------------
 function App() {
   const [search, setSearch] = useState("");
-  const [results, setResults] = useState<ApiData[]>([]);
+  const [results, setResults] = useState<ApiData>({
+    totalPrice: 0,
+    data: []
+  });
 
   async function handleSearch(event: FormEvent) {
     event.preventDefault();
@@ -27,7 +33,27 @@ function App() {
 
     const data = await response.json();
 
-    setResults(data);
+    const formatter = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+    const formatedProducts = data.map((product: any) => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: formatter.format(product.price)
+      }
+    })
+
+    const totalPrice = data?.reduce((total: any, product: any) => {
+      return total + product.price;
+    }, 0);
+
+    setResults({
+      totalPrice,
+      data: formatedProducts,
+    });
   }
 
   const addToWishlist = useCallback((id: number) => {
@@ -47,7 +73,11 @@ function App() {
         <button type="submit">Buscar</button>
       </form>
 
-      <SearchResults addToWishlist={addToWishlist} results={results} />
+      <SearchResults
+        addToWishlist={addToWishlist}
+        results={results.data}
+        totalPrice={results.totalPrice}
+      />
     </div>
   );
 }
